9d2d4bd668eca155aec0bb51d9b270d936019bb0
Kat Perreira <kat.perreira@gmail.com>
2018-08-14 20:35:57 -0700

    Passed facecard test

--- lib/blackjack_score.rb
+++ lib/blackjack_score.rb
@@ -3,5 +3,21 @@
 VALID_CARDS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 'King', 'Queen', 'Jack']
 
 def blackjack_score(hand)
+  facecards_converted = []
+  aces_converted = []
 
+  hand.each do |element|
+    case element
+    when "King", "Queen", "Jack"
+      value = 10
+      facecards_converted << value.to_i
+    when 1, 2, 3, 4, 5, 6, 7, 8, 9
+      facecards_converted << element.to_i
+    end
+  end
+
+  hand_case = facecards_converted.sum
+
+
+  return hand_case
 end
