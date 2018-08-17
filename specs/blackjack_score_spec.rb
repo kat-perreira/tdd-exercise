9d40df0a07af4a49aa0b85d175d36ff8d22a7836
Kat Perreira <kat.perreira@gmail.com>
2018-08-16 20:47:08 -0700

    Additional test for hand limit

--- specs/blackjack_score_spec.rb
+++ specs/blackjack_score_spec.rb
@@ -12,7 +12,7 @@ describe 'Blackjac Score' do
   it 'can calculate the score for a pair of number cards' do
 
     # Arrange
-    hand = [3, 4]
+    hand = [ 3, 4 ]
 
     # Act
     score = blackjack_score(hand)
@@ -53,7 +53,7 @@ describe 'Blackjac Score' do
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
 
     # Arrange
-    hand = ['King', 3, 1] # 10 + 3 + 1 = 14
+    hand = [ 'King', 3, 1 ] # 10 + 3 + 1 = 14
     # Act
     score = blackjack_score(hand)
     # Assert
@@ -63,7 +63,7 @@ describe 'Blackjac Score' do
   it 'raises an ArgumentError for invalid cards' do
 
     # Arrange
-    hand = ["banana", 4, 1]
+    hand = [ 'banana', 4, 1 ]
     # Act  Assert
     expect do
       blackjack_score(hand)
@@ -72,7 +72,7 @@ describe 'Blackjac Score' do
 
   it 'raises an ArgumentError for scores over 21' do
     # Arrange
-    hand = ['King', 'Queen', 5, 7]
+    hand = [ 'King', 'Queen', 5, 7 ]
     #Act Assert  call the method within the do...end block
     expect do
       blackjack_score(hand)
@@ -81,6 +81,12 @@ describe 'Blackjac Score' do
 
   it 'raises an ArgumentError for hand with more than 5 cards' do
 
+    #Arrange
+    hand = [ 2, 2, 2, 2, 3, 4 ]
 
+    #Act #Assert
+    expect do
+      blackjack_score(hand)
+    end.must_raise ArgumentError
   end
 end
