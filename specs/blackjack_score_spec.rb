bbf82eea7b4ec76311cc6cce78471a6e69c534d4
Kat Perreira <kat.perreira@gmail.com>
2018-08-16 20:58:40 -0700

    Refactoring for Ruby style

--- specs/blackjack_score_spec.rb
+++ specs/blackjack_score_spec.rb
@@ -10,48 +10,33 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 describe 'Blackjac Score' do
   it 'can calculate the score for a pair of number cards' do
-
     # Arrange
     hand = [ 3, 4 ]
-
     # Act
     score = blackjack_score(hand)
-
     # Assert <-  You do this part!
     expect(score).must_equal(7)
-
-  end
+    end
 
   it 'facecards have values calculated correctly' do
-
     # Arrange
-
     hand = [ 'Queen', 2, 2 ]
     # Act
     score = blackjack_score(hand)
-
     # Assert
     expect(score).must_equal(14)
-
-
   end
 
   it 'calculates aces as 11 where it does not go over 21' do
-
     # Arrange
     hand = [ 1, 3, 4 ]
-
     # Act
     score = blackjack_score(hand)
-
     # Assert
     expect(score).must_equal(18)
-
-
   end
 
   it 'calculates aces as 1, if an 11 would cause the score to go over 21' do
-
     # Arrange
     hand = [ 'King', 3, 1 ] # 10 + 3 + 1 = 14
     # Act
@@ -61,7 +46,6 @@ describe 'Blackjac Score' do
   end
 
   it 'raises an ArgumentError for invalid cards' do
-
     # Arrange
     hand = [ 'banana', 4, 1 ]
     # Act  Assert
@@ -80,10 +64,8 @@ describe 'Blackjac Score' do
   end
 
   it 'raises an ArgumentError for hand with more than 5 cards' do
-
     #Arrange
     hand = [ 2, 2, 2, 2, 3, 4 ]
-
     #Act #Assert
     expect do
       blackjack_score(hand)
